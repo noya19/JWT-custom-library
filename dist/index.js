9ce483c768776e18e0b12231028e22768a8bbbf4
Ayon Sharma <ayon@techvariable.com>
2023-06-22 12:27:51 +0530

    change in hashing algo

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,15 @@
-import crypto from 'crypto';
+import { createHmac } from 'crypto';
 async function createHash(payload, saltRounds, secret) {
     return new Promise((rs, rj) => {
-        crypto.pbkdf2(payload, secret, 64, saltRounds, 'sha256', (err, key) => {
-            if (err) {
-                rj(err);
-            }
-            else {
-                rs(key.toString('base64'));
-            }
-        });
+        // crypto.pbkdf2(payload, secret, 64, saltRounds, 'sha256', (err, key) => {
+        //     if (err) {
+        //         rj(err);
+        //     } else {
+        //         rs(key.toString('base64url'));
+        //     }
+        // });
+        const hmac = createHmac('sha256', secret);
+        rs(hmac.update(payload).digest("hex"));
     });
 }
 let signature = '';
@@ -26,21 +27,22 @@ let signature = '';
  *  iv. Compare the Recieved Hash with the Computed Hash
  */
 async function signer(header, payload, secret) {
-    const header_b64 = Buffer.from(JSON.stringify(header), "binary").toString("base64");
-    const payload_b64 = Buffer.from(JSON.stringify(payload), "binary").toString("base64");
-    console.log(payload_b64);
+    const header_b64 = Buffer.from(JSON.stringify(header)).toString("base64url");
+    const payload_b64 = Buffer.from(JSON.stringify(payload)).toString("base64url");
+    // console.log(payload_b64);
     // hash using the plaintext payload.
-    const hashed_payload_b64 = await createHash(JSON.stringify(payload), 10, secret);
+    const hashed_payload_b64 = await createHash(header_b64 + "." + payload_b64, 10, secret);
     // assign signature
     signature = hashed_payload_b64;
     // JWT 
+    console.log("Payload -->", header_b64 + '.' + payload_b64 + '.' + hashed_payload_b64);
     return header_b64 + '.' + payload_b64 + '.' + hashed_payload_b64;
 }
 async function verifier(JWT, secret) {
     const [header, payload, signature] = JWT.split('.');
-    const payload_text = Buffer.from(payload, "binary").toString("ascii");
-    console.log(payload_text);
-    const test_signature = await createHash(payload_text, 10, secret);
+    // const payload_text = Buffer.from(header+"."+payload, "base64").toString("ascii");
+    // console.log(payload_text);
+    const test_signature = await createHash(header + "." + payload, 10, secret);
     return signature === test_signature;
 }
 const JWT = await signer({ alg: 'sha256', typ: 'JWT' }, { id: '123' }, 'xyz');
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import crypto from 'crypto';
+import crypto, { createHmac } from 'crypto';
 
 async function createHash(
     payload: string,
@@ -6,13 +6,15 @@ async function createHash(
     secret: string
 ): Promise<string> {
     return new Promise((rs, rj) => {
-        crypto.pbkdf2(payload, secret, 64, saltRounds, 'sha256', (err, key) => {
-            if (err) {
-                rj(err);
-            } else {
-                rs(key.toString('base64'));
-            }
-        });
+        // crypto.pbkdf2(payload, secret, 64, saltRounds, 'sha256', (err, key) => {
+        //     if (err) {
+        //         rj(err);
+        //     } else {
+        //         rs(key.toString('base64url'));
+        //     }
+        // });
+        const hmac = createHmac('sha256', secret);
+        rs(hmac.update(payload).digest("hex"));
     });
 }
 
@@ -38,26 +40,27 @@ let signature = '';
  */
 
 async function signer(header: header, payload: Object, secret: string) {
-    const header_b64 = Buffer.from(JSON.stringify(header)).toString("base64");
-    const payload_b64 = Buffer.from(JSON.stringify(payload)).toString("base64");
-    console.log(payload_b64);
+    const header_b64 = Buffer.from(JSON.stringify(header)).toString("base64url");
+    const payload_b64 = Buffer.from(JSON.stringify(payload)).toString("base64url");
+    // console.log(payload_b64);
 
     // hash using the plaintext payload.
-    const hashed_payload_b64 = await createHash(JSON.stringify(payload), 10, secret);
+    const hashed_payload_b64 = await createHash(header_b64 + "." + payload_b64, 10, secret);
 
     // assign signature
     signature = hashed_payload_b64;
 
     // JWT 
+    console.log("Payload -->", header_b64 + '.' + payload_b64 + '.' + hashed_payload_b64);
     return header_b64 + '.' + payload_b64 + '.' + hashed_payload_b64;
 }
 
 async function verifier(JWT: string, secret: string) {
     const [header, payload, signature] = JWT.split('.');
-    const payload_text = Buffer.from(payload, "base64").toString("ascii");
-    console.log(payload_text);
+    // const payload_text = Buffer.from(header+"."+payload, "base64").toString("ascii");
+    // console.log(payload_text);
 
-    const test_signature = await createHash(payload_text, 10, secret);
+    const test_signature = await createHash(header + "." + payload, 10, secret);
 
     return signature === test_signature;
 }
